ad61a3c1b1f02254b99f02fe140e9f6f7d1394ce
AREZKI BAZIZI <144291687+ArezkiBazizi@users.noreply.github.com>
2023-11-15 14:19:40 +0100

    Add files via upload

---
+++ src/pages/content.js
@@ -0,0 +1,148 @@
+// Personal Information --> #hero section
+export const name = "Arezki Bazizi"
+export const designation = "Developpeur Web"
+export const location = "France"
+export const website = "https://github.com/ArezkiBazizi/ArezkiBazizi"
+
+// About
+export const Bio = "Bienvenue dans Portfolio, cette petite brèche spatio-temporelle qui vous donne un aperçu sur mon univers créatif. Passioné par les technologies du numérique, et curieux de nature, j'ai aqcuis par le biais de mes efforts personnels et de mon encadrement académique des connaisances dans divers domaine informatique : Dev Web Full-Stack, Programataion Orienté Object, Data Science et Inteligence Artificielle.. "
+
+// Work Experience --> #work section
+export const stages = [
+	{
+		from: "fév 2023",
+		to: "Mars 2023",
+		role: "Prédiction de la résiliation de contrat des clients",
+		org: "Ooredoo Algérie",
+		org_url: "https://www.ooredoo.dz/fr/",
+		location: "Bejaia, Algerie",
+		skills: "Data Mining : Collecter, analyser et préparer les données des clients. Construction du modèle d'apprentissage : Knn et régression logistique. Prédiction et Conclusion : Comparer les performances des deux méthodes en termes d'exactitude."
+	},
+
+	{
+		from: "Déc 2022",
+		to: "Jan 2023",
+		role: "Architecte bases de données",
+		org: "SONATRACH",
+		org_url: "https://sonatrach.com/",
+		location: "Bejaia, Algeie",
+		skills: " L'étude des normes, standards et procédures du service HSE, Étude du SI existant au sein du département, Conception et l'implémentation d'une solution base de données pour la gestion et la maintenance du service HSE, Implémentation d'une interface graphique avec Python"
+	}
+]
+
+// Projects --> #project section
+export const projects = [
+	{
+		title: "Project Griffin",
+		description: "A project to improve safety standards in Quahog by implementing innovative safety measures.",
+		url: "https://projectgriffin.com",
+		tech: "Safety Equipment, Training Programs, Community Outreach",
+	},
+	{
+		title: "Family Safety Campaign",
+		description: "An initiative to promote safety awareness among families in Quahog.",
+		url: "https://safefamilyquahog.com",
+		tech: "Workshops, Educational Material, Public Events",
+	},
+	{
+		title: "Road Safety Initiative",
+		description: "A campaign to enhance road safety and reduce accidents in Quahog.",
+		url: "https://roadsafetyquahog.com",
+		tech: "Traffic Analysis, Signage Improvement, Public Awareness",
+	}
+]
+
+// Education --> #education section
+export const formation = [
+	{
+		from: "2023",
+		to: "2024",
+		institute: "Université de Haute-Alsace",
+		course: "Licence informatique option MIAGE",
+		location: "Mulhouse, France"
+	},
+	{
+		from: "2022",
+		to: "2023",
+		institute: "Université de Abderahmane Mira",
+		course: "Master informatioque : Systèmes d'Informations Avancées",
+		location: "Bejaia, Algerie"
+	},
+	{
+		from: "2019",
+		to: "2022",
+		institute: "Université de Abderahmane Mira",
+		course: "Licence informatioque option Système d'Informations",
+		location: "Bejaia, Algerie"
+	}
+]
+
+// Contact --> #contact section
+export const contact = [
+	{
+		title: "Email",
+		description: "baziziarezki@gmail.com",
+	},
+	{
+		title: "Whatapp",
+		description: "+33769614503",
+	},
+	{
+		title : "Profil LinkedIn",
+		source_name: "LinkedIn",
+		source: "https://www.linkedin.com/in/arezki-bazizi-recherche-stage-informatique-d%C3%A9veloppement-web-alternance-/",
+	},
+]
+
+// Certifications --> #certificate section
+
+export const certificates = [
+	{
+		issued: "2005",
+		title: "Certified Safety Inspector",
+		org: "National Safety Association",
+		url: "https://certi.com/url",
+		description: "Certification in safety inspection and risk assessment.",
+	},
+	{
+		issued: "2006",
+		title: "Emergency Response Training",
+		org: "Quahog Safety Department",
+		url: "https://certi.com/url",
+		description: "Training in handling emergency situations and crisis management.",
+	},
+	{
+		issued: "2007",
+		title: "Safety Leadership Workshop",
+		org: "Safety Leaders International",
+		url: "https://certi.com/url",
+		description: "Leadership training in safety management and communication.",
+	}
+]
+
+// Blogs --> #blogs section
+
+
+// Achievements --> #achievement section
+export const Skills = [
+	{
+		title: "Safety Hero Award",
+		year: "2020",
+		decription: "Recognized for outstanding contributions to improving safety standards in Quahog."
+	},
+	{
+		title: "Safety Innovation Prize",
+		year: "2018",
+		decription: "Awarded for implementing innovative safety measures that yielded significant improvements."
+	},
+	{
+		title: "Community Safety Ambassador",
+		year: "2015",
+		decription: "Honored for promoting safety awareness and education in the Quahog community."
+	},
+	{
+		title: "Safety Campaign Excellence",
+		year: "2013",
+		decription: "Acknowledged for leading a successful safety campaign that reached thousands of residents."
+	}
+]
